fb627db15d2bb7dc1a35ea969c1a5878dfa47a6c
djmrskurenai-ctrl <djmrskurenai@gmail.com>
2026-06-09 19:16:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+# demás rutas...
+
+if __name__ == "__main__":
+    app.run(debug=True)
